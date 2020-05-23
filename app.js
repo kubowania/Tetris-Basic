033ba5b53350ec96d510d4ea138edef320fd4fc1
aerinkayne <39535726+aerinkayne@users.noreply.github.com>
2020-05-23 00:34:55 -0400

    rotation check function
    Hi Ania.  I really enjoyed your tutorial on youtube and found it very helpful.  I think I have a fix for rotation sometimes causing pieces to cross the screen.  It could probably be more elegant but it seems to work (I included left/right edge checks as functions because it was easier for me that way).

--- app.js
+++ app.js
@@ -139,6 +139,30 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  
+  function isAtRight() {
+      return current.some(index=> (currentPosition + index + 1) % width === 0);  
+  }
+  function isAtLeft() {
+      return current.some(index=> (currentPosition + index) % width === 0);
+  }
+  function checkRotatedPosition(P){
+      P = P || currentPosition;        //get current position.  Then, check if the piece is near the left side.
+      if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
+          if (isAtRight()){            //use actual position to check if it's flipped over to right side
+              currentPosition += 1;    //if so, add one to wrap it back around
+              checkRotatedPosition(P); //check again.  Pass position from start, since long block might need to move more.
+          }
+      }
+      else if (P % width > 5) {
+          if (isAtLeft()){
+              currentPosition -= 1;
+              checkRotatedPosition(P);
+          }
+      }
+  }
+  
+  
   //rotate the tetromino
   function rotate() {
     undraw()
@@ -147,9 +171,13 @@ document.addEventListener('DOMContentLoaded', () => {
       currentRotation = 0
     }
     current = theTetrominoes[random][currentRotation]
+    checkRotatedPosition();
     draw()
   }
+  
 
+  
+  
   //show up-next tetromino in mini-grid display
   const displaySquares = document.querySelectorAll('.mini-grid div')
   const displayWidth = 4
